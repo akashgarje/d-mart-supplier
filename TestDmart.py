24f1384405a7e89a6791a0522ca0ce23178cd264
Akash Garje <akash15484@mastek.com>
2022-05-22 14:44:12 +0530

    function description added

--- TestDmart.py
+++ TestDmart.py
@@ -8,6 +8,7 @@ def instance():
     return csvtojson
 
 
+# test to check if provided file is .csv:
 def test_Extension(instance):
     csv_file = instance.data_file
     instance.main(csv_file=csv_file)
@@ -17,6 +18,7 @@ def test_Extension(instance):
         raise Exception("File Provided is not CSV")
 
 
+# test to check if main function runs properly:
 def test_function(instance):
     csv_file = instance.data_file
     try:
@@ -25,7 +27,6 @@ def test_function(instance):
         print("CsvToJson Not Defined")
     else:
         print("Successful!")
-    # data = 'data.csv'
-    # assert CsvToJson.main(data) == instance.main(data), "Error"
+
 
 
